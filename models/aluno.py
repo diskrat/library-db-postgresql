c20e9a05838bacbde6986cb2646368c14f3d5c1d
Lucas Ferreira <lucas.ferreira@geowellex.com>
2025-06-25 10:31:48 -0300

    feat: add new strucuture and fix some wrong behaviors

--- models/aluno.py
+++ models/aluno.py
@@ -1,37 +1,12 @@
-from typing import List, Optional
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import ForeignKey
+
+Base = declarative_base()
 
 class Aluno(Base):
-    __tablename__ = "user_account"
-    matricula: Mapped[int] = mapped_column(primary_key=True)
+    __tablename__ = "aluno"
+    matricula: Mapped[int] = mapped_column(ForeignKey("aluno.matricula"), primary_key=True)
     nome: Mapped[str]
     email: Mapped[str]
     curso: Mapped[str]
-
-class Livro(Base):
-    __tablename__ = "livro"
-    codigo: Mapped[int] = mapped_column(primary_key=True)
-    titulo: Mapped[str]
-    autor: Mapped[str]
-    editora: Mapped[str]
-    ano_publicacao: Mapped[int]
-
-class Exemplar(Base):
-    __tablename__ = "exemplar"
-    tombo: Mapped[int] = mapped_column(primary_key=True)
-    codigo_livro: Mapped[int] = mapped_column(ForeignKey("livro.codigo"))
-
-class Emprestimo(Base):
-    __tablename__ = "emprestimo"
-    codigo: Mapped[int] = mapped_column(primary_key=True)
-    data_emprestimo: Mapped[date]
-    data_prev_devolucao: Mapped[date]
-    data_devolucao: Mapped[date]
-    matricula_aluno: Mapped[int] = mapped_column(ForeignKey("aluno.matricula"))
-
-class EmprestimoExemplar(Base):
-    __tablename__ = "emprestimo_exemplar"
-    codigo_emprestimo: Mapped[int] = mapped_column(ForeignKey("emprestimo.codigo"))
-    tombo_exemplar: Mapped[int] = mapped_column(ForeignKey("exemplar.tombo"))
-
-    
---
+++ models/livro.py
@@ -0,0 +1,12 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, declarative_base
+
+Base = declarative_base()
+
+class Livro(Base):
+    __tablename__ = "livro"
+    codigo: Mapped[int] = mapped_column(ForeignKey("livro.codigo"), primary_key=True)
+    titulo: Mapped[str]
+    autor: Mapped[str]
+    editora: Mapped[str]
+    ano: Mapped[int]
